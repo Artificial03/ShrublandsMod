7c03356d38cdf700dd37f34fd8469d95be25bca8
Delta O <deltaoliver@gmail.com>
2022-12-17 19:35:47 +0800

    Addition of jarrah wood and jarrah wood sword : textureless but ready for textures (with potentail defaults modifications)

---
+++ Content/Items/Placeable/JarrahWood.cs
@@ -0,0 +1,48 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+
+namespace ShrublandsMod.Content.Items.Placeable
+{
+    public class JarrahWood : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 12;
+            Item.height = 12;
+            Item.maxStack = 999;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.createTile = ModContent.TileType<Tiles.JarrahWood>();
+        }
+
+        // Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+        //public override void AddRecipes()
+        //{
+        //    CreateRecipe(10)
+        //        .AddIngredient<ExampleItem>()
+        //        .AddTile<Tiles.Furniture.ExampleWorkbench>()
+        //        .Register();
+
+        //    CreateRecipe() // Add multiple recipes set to one Item.
+        //        .AddIngredient<ExampleWall>(4)
+        //        .AddTile<Tiles.Furniture.ExampleWorkbench>()
+        //        .Register();
+
+        //    CreateRecipe()
+        //        .AddIngredient<ExamplePlatform>(2)
+        //        .AddTile<Tiles.Furniture.ExampleWorkbench>()
+        //        .Register();
+        //}
+    }
+}
---
+++ Content/Tiles/JarrahWood.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ShrublandsMod.Content.Tiles
+{
+    public class JarrahWood : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileBlockLight[Type] = true;
+
+            DustType = DustID.RichMahogany;
+            ItemDrop = ModContent.ItemType<Items.Placeable.JarrahWood>();
+
+            AddMapEntry(new Color(215, 78, 52));
+        }
+    }
+}
