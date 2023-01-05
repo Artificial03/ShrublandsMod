82b93efe72bd9c6e5e070f5d058fc4a5b09894da
Delta O <deltaoliver@gmail.com>
2023-01-05 13:43:07 +0800

    New grass. Doesnt spread that fast, doesnt overtake surface. Basis for a grass seed.

---
+++ Content/Items/Plants/ShrublandsGrassSeed.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+
+namespace ShrublandsMod.Content.Items.Plants
+{
+    public class ShrublandsGrassSeed : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Can be placed");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100;
+
+        }
+
+        public override void SetDefaults()
+        {
+            Item.autoReuse = true;
+            Item.useTurn = true;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useAnimation = 15;
+            Item.rare = ItemRarityID.Blue;
+            Item.useTime = 15;
+            Item.maxStack = 99;
+            Item.placeStyle = 0;
+            Item.width = 22;
+            Item.height = 20;
+            Item.value = Item.buyPrice(0, 0, 5, 0);
+        }
+
+        //public override bool UseItem(Player player)
+        //{
+        //    if (Main.netMode == NetmodeID.Server)
+        //        return false;
+
+        //    Tile tile = Framing.GetTileSafely(Player.tileTargetX, Player.tileTargetY);
+        //    if (tile.active() && tile.TileType == TileID.Dirt && player.WithinPlacementRange(Player.tileTargetX, Player.tileTargetY))
+        //    {
+        //        WorldGen.PlaceTile(Player.tileTargetX, Player.tileTargetY, ModContent.TileType<Tiles.ShrublandsGrass>(), forced: true);
+        //        player.inventory[player.selectedItem].stack--;
+        //    }
+
+        //    return true;
+        //}
+    }
+}
---
+++ Content/Tiles/ShrublandsGrass.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+
+
+namespace ShrublandsMod.Content.Tiles
+{
+    public class ShrublandsGrass : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMerge[Type][ModContent.TileType<ShrublandsGrass>()] = true;
+            Main.tileBlendAll[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileBlockLight[Type] = true;
+
+            TileID.Sets.Grass[Type] = true;
+            TileID.Sets.NeedsGrassFraming[Type] = true;
+            TileID.Sets.NeedsGrassFramingDirt[Type] = TileID.Dirt;
+            TileID.Sets.Conversion.Grass[Type] = true;
+
+            AddMapEntry(new Color(45, 51, 20));
+            //SetModTree(new ReachTree());
+            ItemDrop = ItemID.DirtBlock;
+        }
+
+        public override void RandomUpdate(int i, int j)
+        {
+            Tile tile = Framing.GetTileSafely(i, j);
+            Tile tileBelow = Framing.GetTileSafely(i, j + 1);
+            Tile tileAbove = Framing.GetTileSafely(i, j - 1);
+
+            //Try place vine
+            //if (WorldGen.genRand.NextBool(15) && !tileBelow.active() && !tileBelow.lava())
+            //{
+            //    if (!tile.bottomSlope())
+            //    {
+            //        tileBelow.type = (ushort)ModContent.TileType<BriarVines>();
+            //        tileBelow.active(true);
+            //        WorldGen.SquareTileFrame(i, j + 1, true);
+            //        if (Main.netMode == NetmodeID.Server)
+            //        {
+            //            NetMessage.SendTileSquare(-1, i, j + 1, 3, TileChangeType.None);
+            //        }
+            //    }
+            //}
+
+            ////try place foliage
+            //if (WorldGen.genRand.NextBool(25) && !tileAbove.active() && !tileBelow.lava())
+            //{
+            //    if (!tile.bottomSlope() && !tile.topSlope() && !tile.halfBrick() && !tile.topSlope())
+            //    {
+            //        tileAbove.type = (ushort)ModContent.TileType<BriarFoliage>();
+            //        tileAbove.active(true);
+            //        tileAbove.frameY = 0;
+            //        tileAbove.frameX = (short)(WorldGen.genRand.Next(8) * 18);
+            //        WorldGen.SquareTileFrame(i, j + 1, true);
+            //        if (Main.netMode == NetmodeID.Server)
+            //            NetMessage.SendTileSquare(-1, i, j - 1, 3, TileChangeType.None);
+            //    }
+            //}
+
+            //// Try place super sunflower
+            //if (Main.hardMode && WorldGen.genRand.NextBool(500))
+            //    if (WorldGen.PlaceTile(i, j - 1, ModContent.TileType<SuperSunFlower>(), true))
+            //        MyWorld.superSunFlowerPositions.Add(new Point16(i, j - 1));
+
+            //Try spread grass
+            if (Main.rand.NextBool(Main.dayTime && j < Main.worldSurface ? 5 : 8))
+            {
+                List<Point> adjacents = OpenAdjacents(i, j, TileID.Dirt);
+                if (adjacents.Count > 0)
+                {
+                    Point p = adjacents[Main.rand.Next(adjacents.Count)];
+                    if (HasOpening(p.X, p.Y))
+                    {
+                        Framing.GetTileSafely(p.X, p.Y).TileType = (ushort)ModContent.TileType<Tiles.ShrublandsGrass>();
+                        if (Main.netMode == NetmodeID.Server)
+                            NetMessage.SendTileSquare(-1, p.X, p.Y, 1, TileChangeType.None);
+                    }
+                }
+            }
+        }
+
+        private List<Point> OpenAdjacents(int i, int j, int type)
+        {
+            var p = new List<Point>();
+            for (int k = -1; k < 2; ++k)
+                for (int l = -1; l < 2; ++l)
+                    if (!(l == 0 && k == 0) && Framing.GetTileSafely(i + k, j + l).HasTile && Framing.GetTileSafely(i + k, j + l).TileType == type)
+                    {
+                        p.Add(new Point(i + k, j + l));
+                    }
+
+            return p;
+        }
+
+        private bool HasOpening(int i, int j)
+        {
+            for (int k = -1; k < 2; ++k)
+                for (int l = -1; l < 2; ++l)
+                    if (!Framing.GetTileSafely(i + k, j + l).HasTile)
+                        return true;
+            return false;
+        }
+
+        //public override int SaplingGrowthType(ref int style)
+        //{
+        //    style = 0;
+        //    return ModContent.TileType<ReachSapling>();
+        //}
+
+        public override void KillTile(int i, int j, ref bool fail, ref bool effectOnly, ref bool noItem)
+        {
+            if (!fail) //Change self into dirt
+            {
+                fail = true;
+                Framing.GetTileSafely(i, j).TileType = TileID.Dirt;
+            }
+        }
+    }
+}
