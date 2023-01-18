ecef2e9a2a34366d6cde17e0ec815737cfb48f53
Delta O <deltaoliver@gmail.com>
2023-01-18 12:32:41 +0800

    Spawn rate increased in biome

---
+++ Common/GlobalNPCs/IncreaseSpawnRates.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using ShrublandsMod.Content.Biomes;
+using ShrublandsMod.Content.NPCs;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ShrublandsMod.Common.GlobalNPCs
+{
+    public class IncreaseSpawnRates : GlobalNPC
+    {
+        public override void EditSpawnRate(Player player, ref int spawnRate, ref int maxSpawns)
+        {
+            if (player.InModBiome(ModContent.GetInstance<ShrublandsBiome>()))
+                {
+                    spawnRate = spawnRate/3;
+                    maxSpawns = maxSpawns * 3; 
+                }
+        }
+    }
+}
